5558383e6d856c8df66372d21c0d0686da181d19
ZAVERYGO <Hockey.nick932@gmail.com>
2024-03-30 17:36:13 +0300

    2

--- src/main/java/com/kozich/KinoMan/model/dto/GenreDto.java
+++ src/main/java/com/kozich/KinoMan/model/dto/GenreDto.java
@@ -1,11 +1,6 @@
 package com.kozich.KinoMan.model.dto;
 
-import com.kozich.KinoMan.model.entity.CatalogEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.OneToMany;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 public class GenreDto {
